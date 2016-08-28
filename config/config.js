841e83396274b5ce04ab2966c136a9b2a0ce88ce
Gino Bustelo <lbustelo@us.ibm.com>
2016-08-27 22:13:22 -0500

    Rename the database from test

--- config/config.js
+++ config/config.js
@@ -1,19 +1,30 @@
 module.exports = {
-  "development": {
-    "username": "test",
-    "password": "test",
-    "database": "test",
-    "host": process.env.DB_HOST || "127.0.0.1",
-    "dialect": "postgres",
-    "migrationStorage": "none",
-    "logging": (process.env.DB_DEBUG==='true')
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": process.env.DB_HOST || "127.0.0.1",
-    "dialect": "postgres",
-    "logging": (process.env.DB_DEBUG==='true')
+  db: {
+    "test": {
+      "username": "test",
+      "password": "test",
+      "database": "giphy_bot_test",
+      "host": process.env.DB_HOST || "127.0.0.1",
+      "dialect": "postgres",
+      "migrationStorage": "none",
+      "logging": (process.env.DB_DEBUG==='true')
+    },
+    "development": {
+      "username": "test",
+      "password": "test",
+      "database": "giphy_bot_dev",
+      "host": process.env.DB_HOST || "127.0.0.1",
+      "dialect": "postgres",
+      "migrationStorage": "none",
+      "logging": (process.env.DB_DEBUG==='true')
+    },
+    "production": {
+      "username": "root",
+      "password": null,
+      "database": "giphy_bot_prod",
+      "host": process.env.DB_HOST || "127.0.0.1",
+      "dialect": "postgres",
+      "logging": (process.env.DB_DEBUG==='true')
+    }
   }
 }
